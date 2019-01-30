77efdb4e977f8995fb6ce63a8272b347e1c7f5ad
Артем <qiwihere@gmail.com>
2019-01-30 19:06:40 +0300

    update

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: python book_bot.py
---
+++ book_bot.py
@@ -0,0 +1,27 @@
+import requests
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+token = '552105005:AAHdiHL9xU3gG4GTkDP44gKkjGy-OIaxI20'
+
+updater = Updater(token=token) # Токен API к Telegram
+dispatcher = updater.dispatcher
+
+
+def startCommand(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text='Привет, давай пообщаемся?')
+
+
+def textMessage(bot, update):
+    response = 'Получил Ваше сообщение: ' + update.message.text
+    bot.send_message(chat_id=update.message.chat_id, text=response)
+
+
+start_command_handler = CommandHandler('start', startCommand)
+text_message_handler = MessageHandler(Filters.text, textMessage)
+
+
+dispatcher.add_handler(start_command_handler)
+dispatcher.add_handler(text_message_handler)
+
+updater.start_polling(clean=True)
+updater.idle()
