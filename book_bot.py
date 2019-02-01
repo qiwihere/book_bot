b23ecdb9e8d4939361eec86354771a809fdf6565
qiwihere <cleoveloA1>
2019-02-01 13:57:18 +0300

    update

--- book_bot.py
+++ book_bot.py
@@ -1,7 +1,7 @@
 import requests
 import json
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from transliterate import translit
+from telegram.ext.dispatcher import run_async
 import rfc6266_parser as rfc6266
 
 token = '552105005:AAHdiHL9xU3gG4GTkDP44gKkjGy-OIaxI20'
@@ -10,17 +10,22 @@ updater = Updater(token=token)
 dispatcher = updater.dispatcher
 
 
-def startCommand(bot, update):
+@run_async
+def start_cmd(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text='Я помогу тебе получить ту книгу, которую ты ищешь. Просто напиши ее название, и я посмотрю, что у меня есть для тебя :)')
 
 
-def textMessage(bot, update):
+@run_async
+def book_query(bot, update):
     r = requests.get('https://flbapi.herokuapp.com/', params={'query': update.message.text})
     if r.content:
         result = json.loads(r.content)
         for book in result:
             r_book = requests.get(book['link'])
             filename = rfc6266.parse_requests_response(r_book).filename_unsafe
+
+            if not filename:
+                continue
 
             book_file = open(filename, 'wb')
             book_file.write(r_book.content)
@@ -32,8 +37,8 @@ def textMessage(bot, update):
         bot.send_message(chat_id=update.message.chat_id, text='Увы, я ничего не нашел')
 
 
-start_command_handler = CommandHandler('start', startCommand)
-text_message_handler = MessageHandler(Filters.text, textMessage)
+start_command_handler = CommandHandler('start', start_cmd)
+text_message_handler = MessageHandler(Filters.text, book_query)
 
 
 dispatcher.add_handler(start_command_handler)
